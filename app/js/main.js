b964ac0465b55fa003535bc9d246303725a72ed2
Mira <mirael902@gmail.com>
2020-01-08 15:01:28 +0200

    changesmodal

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,53 @@
-var swiper = new Swiper('.js-our-friends', {
+new Swiper('.js-our-friends', {
     slidesPerView: 3,
     slidesPerGroup: 3,
     loop: true,
     navigation: {
-        nextEl: '.button__next',
-        prevEl: '.button__prev',
-    }
-    });
+        nextEl: '.js-our-friends-next',
+        prevEl: '.js-our-friends-prev',
+    },
+});
+
+    function toggleMobileMenu() {
+        var btn = document.querySelector('.js-btn-menu');
+        var nav = document.querySelector('.js-header-nav');
+        var body = document.querySelector('body');
+
+        btn.onclick = function() {
+            nav.classList.toggle('active');
+            btn.classList.toggle('active');
+            body.classList.toggle('overflow');
+        };
+    };
+
+    toggleMobileMenu();
+
+    function togglePetsInfo() {
+        var btnPets = document.querySelectorAll('.js-btn-pets');
+        var btnClose = document.querySelectorAll('.js-popup-close');
+
+        btnClose.forEach(el => {
+            el.onclick = function(event) {
+                event.preventDefault();
+
+                var item = document.querySelectorAll('.js-popup');
+
+                item.forEach(i => {
+                    i.classList.remove('active');
+                    // body.classList.remove('active');
+                });
+            };
+        });
+
+        btnPets.forEach(el => {
+            el.onclick = function(event) {
+                event.preventDefault();
+
+                var id = this.dataset.id;
+
+                document.querySelector(`#pets-${id}`).classList.add('active');
+            }; 
+        });
+    };
+
+    togglePetsInfo();
